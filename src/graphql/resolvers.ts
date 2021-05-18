561555fb2727768e3917b60021ee7c0fe8496b18
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-05-18 19:37:33 -0300

    Load and merge all resolvers and types to export to the server

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,9 @@
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import path from 'path'
+
+//
+const resolversArray = loadFilesSync(path.join(__dirname, 'modules', '**', 'resolvers.ts'))
+const resolvers = mergeResolvers(resolversArray)
+
+export default resolvers
