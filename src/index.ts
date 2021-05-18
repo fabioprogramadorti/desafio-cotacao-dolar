c4d919972e9975343a87cc0555490e009e4bda96
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-05-18 19:39:37 -0300

    Import typeDefs and resolvers into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { GraphQLServer } from 'graphql-yoga'
+import resolvers from './graphql/resolvers'
+import typeDefs from './graphql/typeDefs'
 // ... or using `require()`
 // const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`
+// const typeDefs = `
+//   type Query {
+//     hello(name: String): String!
+//   }
+// `
 
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
-}
+// const resolvers = {
+//   Query: {
+//     hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+//   },
+// }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 server.start(() => console.log('Server is running on localhost:4000'))
