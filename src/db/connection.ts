daf7c67beb90a17066b1273d0d0bff9cec9fb450
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-05-20 13:32:25 -0300

    Make mongoDB persist locally

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,26 +1,17 @@
 // Conexão mongodb
 import mongoose from 'mongoose'
 
-const dbHost = process.env.DB_HOST
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
-const dbName = process.env.DB_NAME
+const dbName = process.env.DB_NAME || 'desafio-cotacoes'
 
 export const dbInit = () => {
-
-  console.log(
-    dbHost,
-    dbUser,
-    dbPassword,
-    dbName,
-  )
 
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
 
-  var connectionString = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
+  //var connectionString = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
+  var connectionString = `mongodb://localhost:27017/${dbName}`
 
   mongoose.connect(connectionString, options)
     .then(res => {
